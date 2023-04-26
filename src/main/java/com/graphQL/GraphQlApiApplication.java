9e6ffb30b032c56df40a119d9264e35e5316e76d
Vivek Shinde <vivshind@publicisgroupe.net>
2023-04-26 13:11:05 +0530

    inti

---
+++ src/main/java/com/graphQL/GraphQlApiApplication.java
@@ -0,0 +1,13 @@
+package com.graphQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphQlApiApplication.class, args);
+	}
+
+}
